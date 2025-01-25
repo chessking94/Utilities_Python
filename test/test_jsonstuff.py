c672bcf7013b25bdec9602326a5b82603c5b8906
Ethan Hunt <chessking94@gmail.com>
2025-01-25 06:44:22 -0600

    Convert fake_path to a setUp var

--- test/test_jsonstuff.py
+++ test/test_jsonstuff.py
@@ -6,6 +6,9 @@ from src.jsonstuff import reformat_json
 
 
 class TestReformatJson(unittest.TestCase):
+    def setUp(self):
+        self.fake_path = os.path.normpath('/fake/path')
+
     @patch('os.path.isdir')
     @patch('os.listdir')
     @patch('os.path.isfile')
@@ -22,16 +25,15 @@ class TestReformatJson(unittest.TestCase):
         mock_json_load.return_value = {'key': 'value'}
 
         # perform the actual test and validate results
-        fake_path = os.path.normpath('/fake/path')
         expected_result = [
-            os.path.normpath(f'{fake_path}/file1_reformat.json'),
-            os.path.normpath(f'{fake_path}/file2_reformat.json'),
+            os.path.normpath(f'{self.fake_path}/file1_reformat.json'),
+            os.path.normpath(f'{self.fake_path}/file2_reformat.json'),
         ]
 
-        result = reformat_json(fake_path)
+        result = reformat_json(self.fake_path)
         self.assertEqual(result, expected_result)
-        mock_isdir.assert_called_once_with(fake_path)
-        mock_listdir.assert_called_once_with(fake_path)
+        mock_isdir.assert_called_once_with(self.fake_path)
+        mock_listdir.assert_called_once_with(self.fake_path)
         self.assertEqual(mock_open_file.call_count, 4)  # 2 reads and 2 writes
         self.assertEqual(mock_json_dump.call_count, 2)
 
@@ -54,25 +56,23 @@ class TestReformatJson(unittest.TestCase):
     def test_reformat_json_specific_files(
         self, mock_json_dump, mock_json_load, mock_open_file, mock_isfile, mock_isdir
     ):
-        fake_path = os.path.normpath('/fake/path')
-
         # set up the mocking
         mock_isdir.return_value = True
         mock_isfile.side_effect = lambda filepath: filepath in [
-            os.path.join(fake_path, 'file1.json'),
-            os.path.join(fake_path, 'file2.json')
+            os.path.join(self.fake_path, 'file1.json'),
+            os.path.join(self.fake_path, 'file2.json')
         ]
         mock_json_load.return_value = {'key': 'value'}
 
         expected_result = [
-            os.path.normpath(f'{fake_path}/file1_reformat.json'),
-            os.path.normpath(f'{fake_path}/file2_reformat.json'),
+            os.path.normpath(f'{self.fake_path}/file1_reformat.json'),
+            os.path.normpath(f'{self.fake_path}/file2_reformat.json'),
         ]
 
         # perform the actual test and validate results
-        result = reformat_json(fake_path, files=['file1.json', 'file2.json'])
+        result = reformat_json(self.fake_path, files=['file1.json', 'file2.json'])
         self.assertEqual(result, expected_result)
-        mock_isdir.assert_called_once_with(fake_path)
+        mock_isdir.assert_called_once_with(self.fake_path)
         self.assertEqual(mock_open_file.call_count, 4)  # 2 reads and 2 writes
         self.assertEqual(mock_json_dump.call_count, 2)
 
@@ -85,9 +85,9 @@ class TestReformatJson(unittest.TestCase):
 
         # perform the actual test and validate results
         with self.assertRaises(FileNotFoundError):
-            reformat_json('/fake/path', files=['missing_file.json'])
+            reformat_json(self.fake_path, files=['missing_file.json'])
 
-        mock_isdir.assert_called_once_with('/fake/path')
+        mock_isdir.assert_called_once_with(self.fake_path)
 
     @patch('os.path.isdir')
     @patch('os.listdir')
@@ -97,26 +97,24 @@ class TestReformatJson(unittest.TestCase):
     def test_reformat_json_skips_existing_reformatted_files(
         self, mock_json_load, mock_open_file, mock_isfile, mock_listdir, mock_isdir
     ):
-        fake_path = os.path.normpath('/fake/path')
-
         # set up the mocking
         mock_isdir.return_value = True
         mock_listdir.return_value = ['file1.json', 'file1_reformat.json', 'file2.json']
 
         def isfile_mock(filepath):
-            if filepath == os.path.join(fake_path, 'file1_reformat.json'):
+            if filepath == os.path.join(self.fake_path, 'file1_reformat.json'):
                 return True
             return filepath in [
-                os.path.join(fake_path, 'file1.json'),
-                os.path.join(fake_path, 'file2.json')
+                os.path.join(self.fake_path, 'file1.json'),
+                os.path.join(self.fake_path, 'file2.json')
             ]
         mock_isfile.side_effect = isfile_mock
 
         mock_json_load.return_value = {'key': 'value'}
 
         # perform the actual test and validate results
-        result = reformat_json(fake_path)
-        self.assertEqual(result, [os.path.normpath('/fake/path/file2_reformat.json')])
-        mock_isdir.assert_called_once_with(fake_path)
-        mock_listdir.assert_called_once_with(fake_path)
+        result = reformat_json(self.fake_path)
+        self.assertEqual(result, [os.path.join(self.fake_path, 'file2_reformat.json')])
+        mock_isdir.assert_called_once_with(self.fake_path)
+        mock_listdir.assert_called_once_with(self.fake_path)
         self.assertEqual(mock_open_file.call_count, 2)  # 1 read and 1 write
